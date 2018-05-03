a5a5875e4c335d239c270cd3ab0ec90efe52ff94
Carlos  Castellanos <carcas4444@gmail.com>
2018-05-03 00:37:22 +0000

    todo page skeleton

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,10 +9,14 @@ export class Home extends React.Component{
     render(){
         return (
             <div className="text-center mt-5">
-                <h1>Hello Rigo!</h1>
-                <p><img src={rigoImage} /></p>
-                <a href="#" className="btn btn-success">If you see this green button... bootstrap is working</a>
-                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+                <h1>todos</h1>
+                <ul className="list-group w-50 mx-auto">
+                    <li className="list-group-item ">Cras justo odio</li>
+                    <li className="list-group-item">Dapibus ac facilisis in</li>
+                    <li className="list-group-item">Morbi leo risus</li>
+                    <li className="list-group-item">Porta ac consectetur ac</li>
+                    <li className="list-group-item">Vestibulum at eros</li>
+                </ul>
             </div>
         );
     }
